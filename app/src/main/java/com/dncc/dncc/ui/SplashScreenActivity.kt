3412593510b084f32c5da7871d73930015d4bbad
AfifOsama <afifosama66@gmail.com>
2021-11-24 17:00:47 +0700

    creating an empty splash activity

---
+++ app/src/main/java/com/dncc/dncc/ui/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.dncc.dncc.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dncc.dncc.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
