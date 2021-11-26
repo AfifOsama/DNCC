837f5706f5d6afa5188ed519f8110d44cf365bbc
AfifOsama <afifosama66@gmail.com>
2021-11-26 22:25:04 +0700

    implements a splash screen

--- app/src/main/java/com/dncc/dncc/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/dncc/dncc/ui/SplashScreenActivity.kt
@@ -2,11 +2,14 @@ package com.dncc.dncc.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dncc.dncc.R
+import com.dncc.dncc.MainActivity
+import splitties.activities.start
+
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        start<MainActivity>()
+        finish()
     }
 }
