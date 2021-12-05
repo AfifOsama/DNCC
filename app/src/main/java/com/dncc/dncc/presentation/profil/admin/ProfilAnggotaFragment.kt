ab7f153f72b879941a2a247bd34059936d0e40a9
AfifOsama <afifosama66@gmail.com>
2021-12-06 05:34:11 +0700

    recondition item card ui

---
+++ app/src/main/java/com/dncc/dncc/presentation/profil/admin/ProfilAnggotaFragment.kt
@@ -0,0 +1,25 @@
+package com.dncc.dncc.presentation.profil.admin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dncc.dncc.R
+import com.dncc.dncc.databinding.FragmentProfilAnggotaBinding
+
+class ProfilAnggotaFragment : Fragment() {
+    private var _binding:FragmentProfilAnggotaBinding?=null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding= FragmentProfilAnggotaBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
