8d3e748f482ab7439b8786a916371e8acfbc56e7
AfifOsama <afifosama66@gmail.com>
2021-12-08 14:59:25 +0700

    adding remove button for admin at ListPelatihanFragment

--- app/src/main/java/com/dncc/dncc/presentation/pelatihan/ListPelatihanFragment.kt
+++ app/src/main/java/com/dncc/dncc/presentation/pelatihan/ListPelatihanFragment.kt
@@ -1,9 +1,11 @@
 package com.dncc.dncc.presentation.pelatihan
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.dncc.dncc.databinding.FragmentListPelatihanBinding
@@ -28,6 +30,22 @@ class ListPelatihanFragment : Fragment() {
 
     private fun initiateUI() {
         initiateToolbar()
+    }
+
+    private fun alertDialog() {
+        val dialogBuilder = AlertDialog.Builder(activity)
+        with(dialogBuilder) {
+            setTitle("Peringatan")
+            setMessage("Yakin ingin menghapus pelatihan divisi ini?")
+            setPositiveButton("Iya") { _, _ ->
+                Toast.makeText(activity, "Pelatihan divisi berhasil dihapus", Toast.LENGTH_LONG)
+                    .show()
+            }
+            setNegativeButton("Tidak") { _, _ ->
+
+            }
+            show()
+        }
     }
 
     private fun initiateToolbar() {
