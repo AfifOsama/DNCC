c9524e303dd087643b5d987ee56c358b28e8e8b6
AfifOsama <afifosama66@gmail.com>
2021-12-19 21:36:14 +0700

    adding progressImg to card users, card users admin, home mentor, and home admin

--- app/src/main/java/com/dncc/dncc/presentation/pelatihan/adapter/UsersAdapter.kt
+++ app/src/main/java/com/dncc/dncc/presentation/pelatihan/adapter/UsersAdapter.kt
@@ -2,9 +2,10 @@ package com.dncc.dncc.presentation.pelatihan.adapter
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
+import com.bumptech.glide.Glide
 import com.dncc.dncc.R
 import com.dncc.dncc.databinding.ItemCardAnggotaBinding
 import com.dncc.dncc.domain.entity.user.UserEntity
@@ -28,11 +29,13 @@ class UsersAdapter(
             binding.apply {
                 val imagePath =
                     FirebaseStorage.getInstance().reference.child("images").child(userEntity.userId)
+                progressImg.visibility = View.VISIBLE
                 imagePath.downloadUrl.addOnSuccessListener {
-                    imgUser.load(it.toString()) {
-                        placeholder(R.drawable.logodncc)
-                        error(R.drawable.logodncc)
-                    }
+                    progressImg.visibility = View.GONE
+                    Glide.with(itemView.context)
+                        .load(it)
+                        .error(R.drawable.logodncc)
+                        .into(imgUser)
                 }.addOnFailureListener {
                     it.message?.let { error -> Log.i("HomeFragment", "error image $error") }
                 }
